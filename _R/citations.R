4ad6a05313c076d4ddda74e7c333faf0fa4c2a7a
Peter Solymos <psolymos@gmail.com>
2026-04-26 11:04:39 -0600

    Throttle
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- _R/citations.R
+++ _R/citations.R
@@ -1,14 +1,21 @@
+# when rate limited by google, spin up a VM and use docker:
+# git clone https://github.com/psolymos/psolymos.github.io.git
+# docker pull rocker/r2u
+# docker run -v $(pwd)/cd psolymos.github.io:/psolymos.github.io -it rocker/r2u bash
+# cd psolymos.github.io
+# start R and install.packages(c("yaml","scholar"))
+
 library(scholar)
 library(dplyr)
 library(ggplot2)
 
 id <- "PfC17QsAAAAJ"
 y <- yaml::read_yaml("_data/papers_to_edit.yml")
-pibids <- sapply(y, function(x) {
+pids <- sapply(y, function(x) {
     if (is.null(x$pubid)) NA_character_ else x$pubid
 })
 p0 <- get_publications(id)
-p <- p0[!is.na(p0$pubid) & p0$pubid %in% pibids, ]
+p <- p0[!is.na(p0$pubid) & p0$pubid %in% pids, ]
 sum(p$cites)
 # ct <- get_citation_history(id)
 
@@ -24,6 +31,7 @@ yaml::write_yaml(y, "_data/papers.yml")
 cit <- NULL
 for (i in 1:nrow(p)) {
     message(i)
+    Sys.sleep(5)
     pid <- p$pubid[i]
     tmp <- get_article_cite_history(id, pid)
     cit <- rbind(cit, tmp)
