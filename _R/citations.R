465d6ea8d0aa76e8e66ee65fd07d17996565ea49
Peter Solymos <psolymos@gmail.com>
2026-05-02 11:51:20 -0600

    cit
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- _R/citations.R
+++ _R/citations.R
@@ -29,6 +29,7 @@ for (i in 1:nrow(p)) {
 # new cites
 sum(p$cites)
 # old cites
+sum(p$oldcites)
 
 # ct <- get_citation_history(id)
 
@@ -41,54 +42,104 @@ for (j in 1:length(y)) {
 }
 yaml::write_yaml(y, "_data/papers.yml")
 
+z <- read.csv("_data/citations.csv")
+zl <- lapply(unique(z$pubid), function(pid) {
+    z[z$pubid == pid, ]
+})
+names(zl) <- unique(z$pubid)
+k <- union(p$pubid[p$cites != p$oldcites], z$pubid[!z$pubid %in% p$pubid])
+for (i in seq_along(k)) {
+    message(k[i])
+    Sys.sleep(5)
+    pid <- k[i]
+    tmp <- get_article_cite_history(id, pid)
+    zl[[pid]] <- tmp
+}
+zo <- do.call(rbind, zl)
 sum(p$oldcites)
-if (any(p$cites != p$oldcites)) {
-    k <- p$pubid[p$cites != p$oldcites]
-}
+sum(p$cites)
+sum(zo$cites)
+sum(sapply(zl, function(x) sum(x$cites)))
 
-# throttling requests to google scholar, 5 seconds between requests
-cit <- NULL
-for (i in 1:nrow(p)) {
-    message(i)
-    Sys.sleep(5)
-    pid <- p$pubid[i]
-    tmp <- get_article_cite_history(id, pid)
-    cit <- rbind(cit, tmp)
-}
-# in case it failes, repeat in a day
-for (pid in setdiff(p$pubid, cit$pubid)) {
-    message(pid)
-    Sys.sleep(5)
-    tmp <- get_article_cite_history(id, pid)
-    cit <- rbind(cit, tmp)
-}
-# TODO:
-# only run this piece for pids where the cites changed since the last time
-write.csv(cit, "_data/citations.csv", row.names = FALSE)
+ll <- sapply(zl, function(x) sum(x$cites))
+cc <- data.frame(
+    pid = p$pubid,
+    old = p$oldcites,
+    new = p$cites,
+    list = ll[p$pubid]
+)
+cc <- cc[cc$new > 0, ]
+cc[cc$new != cc$list, ]
+
+write.csv(zo, "_data/citations.csv", row.names = FALSE)
+
+## --- etc ---
 
 z <- read.csv("_data/citations.csv")
 
 # plot publications over time
 yr <- sort(sapply(y, "[[", "year"))
-data.frame(year = yr, n = 1:length(yr)) %>%
+p1 <- data.frame(year = yr) |>
+    group_by(year) |>
+    summarise(n = n()) |>
     ggplot(aes(year, n)) +
-    geom_line() +
-    theme_bw()
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    labs(x = "Year", y = "Number of publications")
+
+p2 <- data.frame(year = yr, n = 1:length(yr)) |>
+    ggplot(aes(year, n)) +
+    geom_step() +
+    theme_bw() +
+    labs(x = "Year", y = "Cumulative number of publications")
 
 # plot citations over time
-z |>
+p3 <- z |>
     group_by(year) |>
     summarise(citations = sum(cites)) |>
     ggplot(aes(year, citations)) +
-    geom_line() +
-    theme_bw()
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    labs(x = "Year", y = "Number of citations")
 
-z |>
+p4 <- z |>
     group_by(year) |>
     summarise(citations = sum(cites)) |>
     mutate(citations = cumsum(citations)) |>
     ggplot(aes(year, citations)) +
     geom_line() +
-    theme_bw()
+    theme_bw() +
+    labs(x = "Year", y = "Cumulative number of citations")
 
-data.frame(cites = rev(sort(p$cites)), id = 1:nrow(p))
+# h-index
+q <- data.frame(cites = rev(sort(p$cites)), id = 1:nrow(p))
+ri <- which.min(abs(q$cites - q$id))
+(h <- (q$id[ri] + q$cites[ri]) / 2)
+p5 <- q |>
+    ggplot(aes(id, cites)) +
+    geom_step() +
+    geom_vline(xintercept = h, linetype = "dashed") +
+    geom_hline(yintercept = h, linetype = "dashed") +
+    theme_bw() +
+    labs(x = "Publication rank", y = "Citations")
+
+ggsave(
+    "images/publications/publications_over_time.png",
+    p1,
+    width = 5,
+    height = 4
+)
+ggsave(
+    "images/publications/cumulative_publications_over_time.png",
+    p2,
+    width = 5,
+    height = 4
+)
+ggsave("images/publications/citations_over_time.png", p3, width = 5, height = 4)
+ggsave(
+    "images/publications/cumulative_citations_over_time.png",
+    p4,
+    width = 5,
+    height = 4
+)
+ggsave("images/publications/h_index.png", p5, width = 5, height = 4)
